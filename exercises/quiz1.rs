4c1be8c59bb90495c95704a85951145315374ea6
Ahmed Radwan <aradwann@proton.me>
2024-06-15 23:34:43 +0300

    solve quiz2

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(apples_no: i32) -> i32 {
+    if apples_no <= 40 {
+        return apples_no * 2;
+    }
+    apples_no
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
